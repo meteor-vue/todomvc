3e40f275ed1e044341fbae07896f8e1c37a8addb
Mitar <mitar.git@tnode.com>
2017-07-09 16:24:10 -0700

    Warn if Symbol.iterator is missing.

--- client/meteor.js
+++ client/meteor.js
@@ -158,40 +158,45 @@ Vue.use({
 // See: https://github.com/meteor/meteor/pull/8888
 // Our fork of Vue supports v-for over iterators.
 // See: https://github.com/vuejs/vue/issues/5893
-if (!LocalCollection.Cursor.prototype[Symbol.iterator]) {
-  LocalCollection.Cursor.prototype[Symbol.iterator] = function () {
-    var self = this;
+if (typeof Symbol !== 'undefined' && Symbol.iterator) {
+  if (!LocalCollection.Cursor.prototype[Symbol.iterator]) {
+    LocalCollection.Cursor.prototype[Symbol.iterator] = function () {
+      var self = this;
 
-    var i = 0;
-    var objects = self._getRawObjects({ordered: true});
+      var i = 0;
+      var objects = self._getRawObjects({ordered: true});
 
-    if (self.reactive) {
-      self._depend({
-        addedBefore: true,
-        removed: true,
-        changed: true,
-        movedBefore: true});
-    }
-
-    return {
-      next: function () {
-        if (i < objects.length) {
-          // This doubles as a clone operation.
-          var elt = self._projectionFn(objects[i++]);
-
-          if (self._transform)
-            elt = self._transform(elt);
-
-          return {
-            value: elt
-          };
-        }
-        else {
-          return {
-            done: true
-          };
-        }
+      if (self.reactive) {
+        self._depend({
+          addedBefore: true,
+          removed: true,
+          changed: true,
+          movedBefore: true});
       }
+
+      return {
+        next: function () {
+          if (i < objects.length) {
+            // This doubles as a clone operation.
+            var elt = self._projectionFn(objects[i++]);
+
+            if (self._transform)
+              elt = self._transform(elt);
+
+            return {
+              value: elt
+            };
+          }
+          else {
+            return {
+              done: true
+            };
+          }
+        }
+      };
     };
-  };
+  }
+}
+else {
+  console.error("Symbol.iterator is not defined. Vue v-for over Minimongo cursors is thus not supported. Is a es6-symbol polyfill missing?");
 }
