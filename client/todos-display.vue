1436adcedbb65b642b3c81b9cd8943708586991a
Mitar <mitar.git@tnode.com>
2017-07-09 02:11:44 -0700

    Extracted todos list into its own component.

--- client/todos-display.vue
+++ client/todos-display.vue
@@ -7,16 +7,7 @@
 
     <section class="main" v-show="all">
       <input class="toggle-all" type="checkbox" id="toggle-all" v-model="allDone"><label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        <li class="todo" v-for="todo in todos" :class="{completed: todo.completed, editing: todo == editedTodo}">
-          <div class="view">
-            <input class="toggle" type="checkbox" :checked="todo.completed" @change="setCompleted(todo, $event.target.checked)">
-            <label @dblclick="editTodo(todo)">{{todo.title}}</label>
-            <button class="destroy" @click="removeTodo(todo)"></button>
-          </div>
-          <input class="edit" type="text" :value="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo, $event.target.value)" @keyup.enter="doneEdit(todo, $event.target.value)" @keyup.esc="cancelEdit(todo)">
-        </li>
-      </ul>
+      <todos-list :todos="todos" />
     </section>
 
     <footer class="footer" v-show="all">
@@ -72,7 +63,6 @@
     data() {
       return {
         newTodo: '',
-        editedTodo: null,
         subHandle: null,
       };
     },
@@ -129,54 +119,9 @@
         this.newTodo = '';
       },
 
-      removeTodo(todo) {
-        // TODO: Handle errors.
-        Methods.Todos.Remove.call({todoId: todo._id});
-      },
-
-      editTodo(todo) {
-        this.beforeEditCache = todo.title;
-        this.editedTodo = todo;
-      },
-
-      doneEdit(todo, title) {
-        if (!this.editedTodo) {
-          return;
-        }
-        this.editedTodo = null;
-        title = title.trim();
-        if (!title) {
-          this.removeTodo(todo);
-        }
-        else {
-          // TODO: Handle errors.
-          Methods.Todos.SetTitle.call({todoId: todo._id, title});
-        }
-      },
-
-      cancelEdit(todo) {
-        this.editedTodo = null;
-        todo.title = this.beforeEditCache;
-      },
-
-      setCompleted(todo, completed) {
-        // TODO: Handle errors.
-        Methods.Todos.SetCompleted.call({todoId: todo._id, completed});
-      },
-
       removeCompleted() {
         // TODO: Handle errors.
         Methods.Todos.RemoveCompleted.call({});
-      },
-    },
-
-    // A custom directive to wait for the DOM to be updated
-    // before focusing on the input field.
-    directives: {
-      'todo-focus'(el, binding) {
-        if (binding.value) {
-          el.focus();
-        }
       },
     },
   }
---
+++ client/todos-list.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul class="todo-list">
+    <li class="todo" v-for="todo in todos" :class="{completed: todo.completed, editing: todo == editedTodo}">
+      <div class="view">
+        <input class="toggle" type="checkbox" :checked="todo.completed" @change="setCompleted(todo, $event.target.checked)">
+        <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+        <button class="destroy" @click="removeTodo(todo)"></button>
+      </div>
+      <input class="edit" type="text" :value="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo, $event.target.value)" @keyup.enter="doneEdit(todo, $event.target.value)" @keyup.esc="cancelEdit(todo)">
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['todos'],
+
+    data() {
+      return {
+        editedTodo: null,
+      };
+    },
+
+    methods: {
+      removeTodo(todo) {
+        // TODO: Handle errors.
+        Methods.Todos.Remove.call({todoId: todo._id});
+      },
+
+      editTodo(todo) {
+        this.beforeEditCache = todo.title;
+        this.editedTodo = todo;
+      },
+
+      doneEdit(todo, title) {
+        if (!this.editedTodo) {
+          return;
+        }
+        this.editedTodo = null;
+        title = title.trim();
+        if (!title) {
+          this.removeTodo(todo);
+        }
+        else {
+          // TODO: Handle errors.
+          Methods.Todos.SetTitle.call({todoId: todo._id, title});
+        }
+      },
+
+      cancelEdit(todo) {
+        this.editedTodo = null;
+        todo.title = this.beforeEditCache;
+      },
+
+      setCompleted(todo, completed) {
+        // TODO: Handle errors.
+        Methods.Todos.SetCompleted.call({todoId: todo._id, completed});
+      },
+    },
+
+    // A custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    directives: {
+      'todo-focus'(el, binding) {
+        if (binding.value) {
+          el.focus();
+        }
+      },
+    },
+  }
+</script>
